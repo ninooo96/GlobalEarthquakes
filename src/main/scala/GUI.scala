80ac778aba1562ebaa340810fe4ea3be127411c8
ninooo96 <ninooo96@gmail.com>
2019-12-24 17:43:06 +0100

    First Gui Commands

--- src/main/scala/GUI.scala
+++ src/main/scala/GUI.scala
@@ -1,9 +1,11 @@
+import multirange.MultiRange
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
-import scalafx.geometry.Insets
+import scalafx.geometry.{Insets, Pos}
 import scalafx.scene.AccessibleRole.TextArea
-import scalafx.scene.Scene
-import scalafx.scene.control.{Label, TextArea, TextField}
+import scalafx.scene.{Node, Scene}
+import scalafx.scene.control.{Button, ComboBox, Label, TextArea, TextField}
+import scalafx.scene.image.{Image, ImageView, WritableImage}
 import scalafx.scene.layout.{AnchorPane, BorderPane, GridPane}
 import scalafx.{application, stage}
 
@@ -38,23 +40,95 @@ object HelloSBT extends JFXApp {
 //stage.setResizable(false)
 
 stage = new PrimaryStage{
-  title = "Prova"
-  scene = new Scene(600,300){
+  title = "Global Earthquakes"
+  scene = new Scene(1200,700){
     val txa = new TextField
     val label = new Label("Ciao")
     val grid = new GridPane
-    grid.add(new Label("antn"),0,0)
-    grid.add(new Label("gaglio"),1,0)
+//    val gridYear = new Grid
+    grid.setHgap(10)
+    grid.setVgap(2)
 
+    grid.add(new Label("Magnitude (Max, Min)"),0,0)
+    grid.add(new GridPane{
+      add(new TextField {
+        promptText = "1"
+        prefColumnCount = 6},0,0)
+      add(new TextField {
+        promptText = "8"
+        prefColumnCount = 6}, 1,0)
+    },0,1)
+
+    grid.add(new Label("Depth (Max, Min)"),0,2)
+    grid.add(new GridPane{
+      add(new TextField {
+        promptText = "0"
+        prefColumnCount = 6},0,0)
+      add(new TextField {
+        promptText = "700"
+        prefColumnCount = 6}, 1,0)
+    },0,3)
+
+    grid.add(new Label("Year "),0,4)
+    grid.add(new GridPane{
+      add(new TextField {
+        promptText = "1900"
+        prefColumnCount = 6},0,0)
+      add(new TextField {
+        promptText = "2018"
+        prefColumnCount = 6}, 1,0)
+    },0,5)
+
+    grid.add(new Label("Year"),1,0)
+    grid.add( new ComboBox(List.range(1900,2019)),1,1)
+
+    grid.add(new Label("Month"),1,2)
+    grid.add( new ComboBox(List.range(1,13)),1,3)
+
+    grid.add(new Label("Day"),1,4)
+    grid.add( new ComboBox(List.range(1,32)),1,5)
+
+    grid.add(new Label("Keyword"),2,0)
+    grid.add(new TextField{promptText="Type keyword"},2,1)
+    grid.add(new Label("Latitude and Longitude"),2,2)
+    grid.add(new GridPane{
+
+      add(new TextField{prefColumnCount = 7},0,0)
+      add(new TextField{prefColumnCount = 7},1,0)
+    },2,3)
+
+    grid.add(new Button {
+      text = "SUBMIT"
+    },3,4)
+    grid.add(new Button {
+      text = "RESET"
+    },3,5)
+
+
+
+
+    val img = new Image("world.png",3*(width/5).toInt, 5*(height/7).toInt/*3*(height/5).toInt*/,false,false)
+    val node = img.pixelReader match {
+      case None => new Label("Image not found")
+      case Some(pr) =>
+//        val wimg = new WritableImage(pr)//, 3*(width/5).toInt, 2*(height/3).toInt )
+        val xScaleRatio = width / img.width.toInt
+        val yScaleRatio = height / img.height.toInt
+        resizable = true
+
+        val wimg = new ImageView(img)
+        wimg
+    }
     root = new AnchorPane {
 
-      AnchorPane.setBottomAnchor(txa, 10)
-      AnchorPane.setTopAnchor(label, 10)
-      AnchorPane.setRightAnchor(txa, 10)
-      AnchorPane.setLeftAnchor(label, 10)
-      AnchorPane.setBottomAnchor(grid,10)
-      AnchorPane.setLeftAnchor(grid,10)
-      children = List(txa, label,grid)
+//      AnchorPane.setBottomAnchor(txa, 10)
+      AnchorPane.setTopAnchor(node, 5)
+      AnchorPane.setLeftAnchor(node, 5)
+//      AnchorPane.setBottomAnchor(grid,5)
+      AnchorPane.setTopAnchor(grid, img.height.toDouble+10)
+//
+      AnchorPane.setLeftAnchor(grid,5)
+      children = List(node,grid)
 
 
     }
@@ -64,3 +138,4 @@ stage = new PrimaryStage{
 
 
 }
+
--- src/main/scala/Gui.java
+++
@@ -1,8 +0,0 @@
-import javafx.fxml.FXML;
-
-import java.awt.*;
-
-public class Gui {
-    @FXML
-    private static TextField key;
-}
