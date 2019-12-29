adeaf262e27061f1f4d8056056b13d86d5ccbb67
ninooo96 <ninooo96@gmail.com>
2019-12-29 21:45:00 +0100

    Final features implemented. Needs some minor fixes

--- src/main/scala/GUI.scala
+++ src/main/scala/GUI.scala
@@ -1,14 +1,21 @@
+import java.awt.image.BufferedImage
+import java.text.NumberFormat
+import java.util.Locale
+
+import javafx.collections.ObservableList
+import javax.imageio.ImageIO
 import scalafx.application.{JFXApp, Platform}
 import scalafx.scene.control.TableColumn._
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.beans.property.{ObjectProperty, StringProperty}
 import scalafx.collections.ObservableBuffer
 import scalafx.event.{ActionEvent, Event}
-import scalafx.scene.{Node, Scene}
-import scalafx.scene.control.{Alert, Button, ComboBox, Label, ProgressBar, TableColumn, TableView, TextArea, TextField}
+import scalafx.scene.{Node, Scene, SnapshotParameters}
+import scalafx.scene.control.{Alert, Button, ComboBox, Label, ProgressBar, TableColumn, TablePosition, TableView, TextArea, TextField}
 import scalafx.scene.image.{Image, ImageView, WritableImage}
 import scalafx.scene.layout.{AnchorPane, BorderPane, GridPane, Pane, StackPane}
 import scalafx.Includes._
+import scalafx.embed.swing.SwingFXUtils
 import scalafx.geometry.Point2D
 import scalafx.scene.canvas.Canvas
 import scalafx.scene.input.MouseEvent
@@ -18,21 +25,40 @@ import scalafx.scene.control.Alert.AlertType
 import scalafx.scene.shape.Rectangle
 import scalafx.stage.FileChooser
 
-case class Print(city: String, region: String, state: String, latitude: String, longitude: String, year: String, month: String, day: String, hour: String, minute: String, depth: String, magnitude: String)
+case class Print(city: String, region: String, state: String, latitude: String, longitude: String, year: String, month: String, day: String, hour: String, minute: String, depth: String, magnitude: String){
+  def getLat() : Double = {
+    val format = NumberFormat.getInstance(Locale.FRANCE)
+    val number = format.parse(latitude)
+    val d = number.doubleValue
+//    println("lat: "+d)
+    return d
+  }
+
+  def getLon() : Double = {
+    val format = NumberFormat.getInstance(Locale.FRANCE)
+    val number = format.parse(longitude)
+    val d = number.doubleValue
+//    println("lon: "+d)
+    return d
+  }
+}
 
 object HelloSBT extends JFXApp {
   val df = Main2.init() //df2
   var query = df//Main2.searchByYear(df,2018)
+  var point2 = Rectangle(0,0,1,1)
 
 
 
   stage = new PrimaryStage {
+    maximized = true
+
     title = "Global Earthquakes"
     scene = new Scene(1200, 700) {
       val txa = new TextField
       val label = new Label("Ciao")
       val grid = new GridPane
-      grid.setHgap(10)
+      grid.setHgap(7)
       grid.setVgap(2)
 
       val magMax = new TextField {
@@ -45,7 +71,7 @@ object HelloSBT extends JFXApp {
       }
 
       val depthMax = new TextField {
-        promptText = "700"
+        promptText = "750"
         prefColumnCount = 6
       }
       val depthMin = new TextField {
@@ -85,6 +111,10 @@ object HelloSBT extends JFXApp {
       }
 
       val longitude = new TextField {
+        prefColumnCount = 7
+      }
+
+      val radius = new TextField {
         prefColumnCount = 7
       }
 
@@ -148,16 +178,20 @@ object HelloSBT extends JFXApp {
         add(latitude, 0, 0)
         add(longitude, 1, 0)
       }, 2, 3)
+      grid.add(new Label("Radius (KM)"),2,4)
+      grid.add(radius,2,5)
 
-      grid.add(submit
-        , 3, 4)
+      grid.add(submit, 3, 4)
       grid.add(reset, 3, 5)
       grid.add(save,3,3)
-      grid.add(resetImg, 4,0)
-      grid.add(saveImg, 4,1)
+      grid.add(resetImg, 4,4)
+      grid.add(saveImg, 4,5)
       val progress = new ProgressBar
       progress.layoutX = 80
       progress.layoutY = 10
+//      grid.add(progress,4,0)
+//      grid.children.-=(progress)
+
 
       val size = new Label()
       val info = new Label()
@@ -175,19 +209,38 @@ object HelloSBT extends JFXApp {
         latitude.text=""
         longitude.text=""
 
-        yearCombo.value = 0
-        monthCombo.value = 0
-        dayCombo.value = 0
+        x_first = 0
+        y_first = 0
+        x_last = 0
+        y_last = 0
+
+        val listTmp = List(yearCombo,monthCombo,dayCombo)
+
+        for (c <- listTmp) {
+          c.value = 0
+        }
       }
 
       resetImg.onAction = (e : ActionEvent) =>{
+        x_first = 0
+        y_first = 0
+        x_last = 0
+        y_last = 0
+        val p = new Point2D(0,0)
+        Updater.update(p, p)
         pane.children = Updater.r
       }
 
       saveImg.onAction = (e : ActionEvent) =>{
-        val fileChooser = new FileChooser
+        val fileChooser = new FileChooser()
         val selectedFile = fileChooser.showSaveDialog(stage)
         //IMPLEMENT SAVE IMAGE
+
+//        selectedFile.createNewFile()
+//        imageViewAdjusted.snapshot(null, null)
+        val img2 = SwingFXUtils.fromFXImage(root2.snapshot(new SnapshotParameters(),null), null)
+
+        ImageIO.write(img2, "png", selectedFile)
       }
 
       save.onAction = (e : ActionEvent) => {
@@ -197,46 +250,65 @@ object HelloSBT extends JFXApp {
       }
 
       submit.onAction = (e:ActionEvent) => {
-        var ok = true
+        var ok = false
         info.text = ""
         var subQuery = df
+
+//        radiusQuery = false
+//        rangeQuery = false
+        val p = new Point2D(0, 0)
+        Updater.update(p, p)
+
         pane.children = Updater.r
+
+        //        grid.children.+=(progress)
 
 
         if (!magMin.getText.equals("") && !magMax.getText.equals("")) {
+          ok = true
           subQuery = Main2.searchByMag(subQuery, magMin.getText.toDouble, magMax.getText.toDouble)
         }
         else if (!magMin.getText.equals("")) {
+          ok = true
           subQuery = Main2.searchByMag(subQuery, minMag = magMin.getText.toDouble)
         }
         else if (!magMax.getText.equals("")) {
+          ok = true
           subQuery = Main2.searchByMag(subQuery, maxMag = magMax.getText.toDouble)
         }
 
         if (!depthMin.getText.equals("") && !depthMax.getText.equals("")) {
+          ok = true
           subQuery = Main2.searchByDepth(subQuery, depthMin.getText.toDouble, depthMax.getText.toDouble)
         }
         else if (!depthMin.getText.equals("")) {
+          ok = true
           subQuery = Main2.searchByDepth(subQuery, minDepth = depthMin.getText.toDouble)
         }
         else if (!depthMax.getText.equals("")) {
+          ok = true
           subQuery = Main2.searchByDepth(subQuery, maxDepth = depthMax.getText.toDouble)
         }
 
         if (!yearMin.getText.equals("") && !yearMax.getText.equals("")) {
+          ok = true
           subQuery = Main2.searchByDateRange(subQuery, minYear = yearMin.getText.toInt, maxYear = yearMax.getText.toInt)
         }
         else if (!yearMin.getText.equals("")) {
+          ok = true
           subQuery = Main2.searchByDateRange(subQuery, minYear = yearMin.getText.toInt)
         }
         else if (!yearMax.getText.equals("")) {
+          ok = true
           subQuery = Main2.searchByDateRange(subQuery, maxYear = yearMax.getText.toInt)
         }
 
         if (!getCombo(yearCombo).equals(0) && !getCombo(monthCombo).equals(0) && !getCombo(dayCombo).equals(0)) {
+          ok = true
           subQuery = Main2.searchByDate(subQuery, getCombo(yearCombo), getCombo(monthCombo), getCombo(dayCombo))
         }
         else if (!getCombo(yearCombo).equals(0) && !getCombo(monthCombo).equals(0)) {
+          ok = true
           subQuery = Main2.searchByMonth(Main2.searchByYear(subQuery, getCombo(yearCombo)), getCombo(monthCombo))
         }
         else if (!getCombo(yearCombo).equals(0) && !getCombo(dayCombo).equals(0)) {
@@ -249,15 +321,18 @@ object HelloSBT extends JFXApp {
           }.showAndWait()
         }
         else if (!getCombo(monthCombo).equals(0) && !getCombo(dayCombo).equals(0)) {
+          ok = true
           subQuery = Main2.searchByDay(Main2.searchByMonth(subQuery, getCombo(monthCombo)), getCombo(dayCombo))
         }
-        else if (!getCombo(yearCombo).equals(0)){
-          subQuery = Main2.searchByYear(subQuery,getCombo(yearCombo))
+        else if (!getCombo(yearCombo).equals(0)) {
+          ok = true
+          subQuery = Main2.searchByYear(subQuery, getCombo(yearCombo))
         }
-        else if (!getCombo(monthCombo).equals(0)){
-          subQuery = Main2.searchByYear(subQuery,getCombo(monthCombo))
+        else if (!getCombo(monthCombo).equals(0)) {
+          ok = true
+          subQuery = Main2.searchByYear(subQuery, getCombo(monthCombo))
         }
-        else if (!getCombo(yearCombo).equals(0)){
+        else if (!getCombo(yearCombo).equals(0)) {
           ok = false
           new Alert(AlertType.Error) {
             initOwner(stage)
@@ -268,10 +343,12 @@ object HelloSBT extends JFXApp {
         }
 
         if (!keyword.getText.equals("")) {
+          ok = true
           subQuery = Main2.searchByKey(subQuery, keyword.getText)
         }
 
         if (!latitude.getText.equals("") && !longitude.getText.equals("")) {
+          ok = true
           subQuery = Main2.searchByLatLon(subQuery, latitude.getText.toDouble, longitude.getText.toDouble)
         }
         else if (latitude.getText.equals("") ^ longitude.getText.equals("")) {
@@ -284,16 +361,114 @@ object HelloSBT extends JFXApp {
           }.showAndWait()
         }
 
+
+        if (!radius.getText.equals("") && radiusQuery) {
+          println (x_first+" "+y_first)
+          ok = true
+          subQuery = Main2.radiusQuery(subQuery, y_first.toDouble, x_first.toDouble, radius.getText.toDouble)
+          }
+
+        else if (!radiusQuery && !radius.getText.equals("")) {
+          ok = false
+          radiusQuery = false
+
+        println(radiusQuery)
+          println (x_first+" "+y_first)
+          new Alert(AlertType.Error) {
+            initOwner(stage)
+            title = "Error Dialog"
+            headerText = "Error"
+            contentText = "You must choose both a location from \nthe map and the radius of the query"
+          }.showAndWait()
+//          x_first = 0
+//          y_first = 0
+//          x_last = 0
+//          y_last = 0
+        }
+        else if (radiusQuery && radius.getText.equals("")) {
+          //        else if (radius.getText.equals("") ^ (x_first == 0.0 && y_first == 0.0)) {
+          ok = false
+          radiusQuery = false
+
+          println (x_first+" "+y_first)
+          new Alert(AlertType.Error) {
+            initOwner(stage)
+            title = "Error Dialog"
+            headerText = "Error"
+            contentText = "You must choose both a location from \nthe map and the radius of the query"
+          }.showAndWait()
+          //          x_first = 0
+          //          y_first = 0
+          //          x_last = 0
+          //          y_last = 0
+        }
+
+
+
+        if( x_first!= 0 && y_first!= 0 && x_last!= 0 && y_last!= 0){
+          //y -> lat
+          //x - lon
+//          println("PROVA: "+y_last + " - "+ x_last)
+          ok = true
+
+          subQuery = Main2.rectQuery(subQuery,y_first,x_first,y_last,x_last)
+          x_first = 0
+          y_first = 0
+          x_last = 0
+          y_last = 0
+        }
+
         if (ok) {
           anchor.children.-=(table)
           table = output(subQuery.collect())
+          table.onMouseClicked = (e : MouseEvent) => {
+            pane.children -= point2
+            val pos = table.getSelectionModel.getSelectedCells.get(0)
+            val row = pos.getRow
+            val item = table.getItems.get(row)
+//            println(item.isInstanceOf[Print])
+//            val row = table.getSelectionModel.getSelectedCells.get
+//            val col = pos.getTableColum/n
+//            val data = col.getCellObservableValue(item.toString).getValue
+//            println(item(0))
+
+            var y_p2 = 0.0
+            var x_p2 = 0.0
+            if(item.getLon() > 0) { //long
+              y_p2 = ((item.getLon() * (canvas.width.toDouble / 2)) / 180) + (canvas.width.toDouble / 2)
+            }
+            else{
+              y_p2 = (canvas.width.toDouble/2 - (Math.abs(item.getLon())*(canvas.width.toDouble/2))/180)//+(canvas.width.toDouble/2)
+            }
+
+            if(item.getLat() > 0) { //lat
+              x_p2 = (item.getLat()*(canvas.height.toDouble/2))/90 + canvas.height.toDouble/2 - 15
+            }
+            else{
+              x_p2 = (canvas.height.toDouble/2 - (Math.abs(item.getLat())*(canvas.height.toDouble/2))/90) //+ (canvas.height.toDouble/2) + 15
+            }
+
+//            println("Lat: "+x_p2 + " - "+"Lon: "+y_p2)
+
+            point2 = Rectangle( y_p2, x_p2 + (canvas.height.toDouble - (x_p2*2)), 3, 3 )
+            point2.fill = Color.Yellow
+            pane.children += point2
+          }
+
+          anchor.children.-=(grid)
+//          grid.children.-=(progress)
+//          anchor.children.+=(grid)
+
+
+
           AnchorPane.setTopAnchor(table, 5)
           AnchorPane.setBottomAnchor(table, 5)
           AnchorPane.setLeftAnchor(table, img.width.toDouble)
           AnchorPane.setRightAnchor(table, 5)
-          anchor.children ++= List(table)
+          anchor.children ++= List(table,grid)
       }
-        grid.children.-=(progress)
+
+
         query = subQuery
       }
 
@@ -344,7 +519,11 @@ object HelloSBT extends JFXApp {
 
       val col12 = new TableColumn[Print, String]("Mag")
 
-
+//      def visualize( row : ObservableList[TablePosition]): Unit ={
+//        val point2 = Rectangle(row(7),0,20,20)
+//        point2.fill = Color.Red
+//        pane.children = += point
+//      }
       private def populateSeq(output : Array[String]) : List[Print]= {
         var seq = List[Print]()
         var paneTmp = pane
@@ -362,20 +541,13 @@ object HelloSBT extends JFXApp {
           }
           else{
             y_p = ((canvas.width.toDouble/2 - (Math.abs(arr(8).toDouble))*(canvas.width.toDouble/2))/180)+(canvas.width.toDouble/2)
-//            y_p = (canvas.width.toDouble/2) - yTmp
-//            println(yTmp + "" + y_p)
-            println(Math.abs(arr(8).toDouble))
           }
 
           if(arr(7).toDouble > 0) { //lat
            x_p = ((arr(7).toDouble)*(canvas.height.toDouble/2))/90 + canvas.height.toDouble/2 - 15
-//            println(x_p)
-            //((arr(7).toDouble)*canvas.height.toDouble)/180
           }
           else{
             x_p = ((canvas.height.toDouble/2 - (Math.abs(arr(7).toDouble))*(canvas.height.toDouble/2))/90) + (canvas.height.toDouble/2) + 15
-            //((arr(7).toDouble)*canvas.height.toDouble)/180
-
           }
 
 
@@ -501,8 +673,8 @@ object HelloSBT extends JFXApp {
 
       object Updater {
 
-        private var _start = new Point2D(-1, -1)
-        private var _end = new Point2D(-1, -1)
+        private var _start = new Point2D(0,0)
+        private var _end = new Point2D(0,0)
 
 //        val canvas = new Canvas(3 * (width / 5).toInt, 5 * (height / 7).toInt)
 //        root2.getChildren.addAll(canvas)
@@ -539,11 +711,11 @@ object HelloSBT extends JFXApp {
     }
 
     // Define handling of mouse events
-    var x_first = -1.0
-    var y_first = -1.0
+    var x_first = 0.0
+    var y_first = 0.0
 
-    var x_last = -1.0
-    var y_last = -1.0
+    var x_last = 0.0
+    var y_last = 0.0
 
     var rangeQuery = false
     var radiusQuery = false
@@ -560,11 +732,18 @@ object HelloSBT extends JFXApp {
               Updater.update(p, p)
 
               //            if(me.x >= canvas.width.toDouble/2){
-              var tmp = (360 * me.x) / canvas.width.toDouble
-              x_first = tmp - 180
 
-              var tmp2 = (180 * me.y) / canvas.height.toDouble
-              y_first = 90 - tmp2
+              var tmp11 = (180 * me.y)/img.height.toDouble
+              y_first = (180 - tmp11 - 90)+5
+
+              var tmp12 = (360*me.x)/img.width.toDouble
+              x_first = tmp12-180
+
+//              var tmp = (360 * me.y) / canvas.width.toDouble
+//              y_first = tmp - 180
+//
+//              var tmp2 = (180 * me.x) / canvas.height.toDouble
+//              x_first = 90 - tmp2
               //            }
               //            var x_first
               println("coord iniziali: " + x_first + " " + y_first+"\n"+"Range = "+rangeQuery+" Radius = "+radiusQuery)
@@ -572,23 +751,35 @@ object HelloSBT extends JFXApp {
             else if (rangeQuery || radiusQuery) {
               radiusQuery = false
               rangeQuery = false
-              val p = new Point2D(-1, -1)
+              val p = new Point2D(0,0)
               Updater.update(p, p)
+              x_first = 0
+              y_first = 0
+              x_last = 0
+              y_last = 0
             }
           }
           case MouseEvent.MouseDragged => {
-//            radiusQuery = false
+            radiusQuery = false
             rangeQuery = true
             // Adjust the shape
             Updater.update(end = new Point2D(me.x, me.y))
+//            if(me.y < canvas.height.toDouble/2) {
+//              var tmp = ( (90 * me.y) / (canvas.height.toDouble/2)) + canvas.height.toDouble/2
+//              y_last = (canvas.height.toDouble/2) - tmp
+//            }
+//            else{
 
-            var tmp = (360*me.x)/canvas.width.toDouble
-            x_last = tmp-180
+//            }
+            var tmp21 = (180 * me.y)/img.height.toDouble
+            y_last = (180 - tmp21 - 90)+5
 
-            var tmp2 = (180*me.y)/canvas.height.toDouble
-            y_last = 90- tmp2
+            var tmp22 = (360*me.x)/img.width.toDouble
+            x_last = tmp22-180
 
-            println("coord finali: "+x_last + " "+y_last)
+
+
+//            println("coord: x_first= "+x_first+" - y_first= "+y_first+" - x_last= "+x_last + " - y_last= "+y_last)
 
           }
           case _                       => {}
@@ -610,7 +801,8 @@ object HelloSBT extends JFXApp {
       var anchor = new AnchorPane {
         AnchorPane.setTopAnchor(root2, 5)
         AnchorPane.setLeftAnchor(root2, 5)
-        AnchorPane.setTopAnchor(grid, img.height.toDouble + 10)
+//        AnchorPane.setTopAnchor(grid, img.height.toDouble + 10)
+        AnchorPane.setBottomAnchor(grid,5)
         AnchorPane.setLeftAnchor(grid, 5)
 
 //        AnchorPane.setLeftAnchor(txtA,img.width.toDouble)
@@ -629,5 +821,6 @@ object HelloSBT extends JFXApp {
       root = anchor
     }
   }
+  stage.show()
 }
 
--- src/main/scala/Main2.scala
+++ src/main/scala/Main2.scala
@@ -31,6 +31,22 @@ object Main2 extends App {
     return df2
   }
 
+  def calculateDistanceInKm(lat1 : Double, lon1 : Double, lat2 : Double, lon2 : Double) : Int ={//userLocation: Location, warehouseLocation: Location): Int = {
+    val AVERAGE_RADIUS_OF_EARTH_KM = 6371
+
+    print("CIAOOO")
+    val latDistance = Math.toRadians(lat1 - lat2)
+    val lngDistance = Math.toRadians(lon1 - lon2)
+    val sinLat = Math.sin(latDistance / 2)
+    val sinLng = Math.sin(lngDistance / 2)
+    val a = sinLat * sinLat +
+      (Math.cos(Math.toRadians(lat1)) *
+        Math.cos(Math.toRadians(lat2)) *
+        sinLng * sinLng)
+    val c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+    return (AVERAGE_RADIUS_OF_EARTH_KM * c).toInt
+  }
+
 
 
     //  for (r <- mapFile) {
@@ -58,11 +74,12 @@ object Main2 extends App {
     //  }
 
     def searchByKey(df2: RDD[String], key: String): RDD[String] = {
-      val query = df2.flatMap(lines => lines.split("\n")).filter(value => value.contains(key))
+      val key2 = key.toLowerCase
+      val query = df2.flatMap(lines => lines.split("\n")).filter(value => value.toLowerCase().contains(key2))
       return query
     }
 
-    def searchByDepth(df2: RDD[String], minDepth: Double = 0, maxDepth: Double = 700): RDD[String] = { //min e max vengono passati come stringhe e poi modificati all'interno della funzione
+    def searchByDepth(df2: RDD[String], minDepth: Double = 0, maxDepth: Double = 750): RDD[String] = { //min e max vengono passati come stringhe e poi modificati all'interno della funzione
       val query = df2
         .flatMap(lines => lines.split("\n")
           .filter(value => value.split(",")(12).toDouble >= minDepth)
@@ -129,6 +146,81 @@ object Main2 extends App {
       .flatMap(lines => lines.split("\n")
         .filter(value => Math.abs(lat - value.split(",")(7).toDouble) <= precision)
         .filter(value =>  Math.abs(lon - value.split(",")(8).toDouble) <= precision)
+      )
+    return query
+  }
+
+  def rectQuery(df2 : RDD[String], x_start : Double, y_start : Double, x_end : Double, y_end : Double) : RDD[String] = {
+    if (x_end >= x_start){
+      if(y_end >= y_start){
+        println("coord: x_first= "+x_start+" - y_first= "+y_start+" - x_last= "+x_end + " - y_last= "+y_end)
+        val query = df2
+          .flatMap(lines => lines.split("\n")
+            .filter(value => value.split(",")(7).toDouble >= x_start)
+            .filter(value => value.split(",")(7).toDouble < x_end)
+            .filter(value => value.split(",")(8).toDouble >= y_start)
+            .filter(value => value.split(",")(8).toDouble < y_end)
+          )
+        return query
+      }
+      else{
+        val query = df2
+          .flatMap(lines => lines.split("\n")
+            .filter(value => value.split(",")(7).toDouble >= x_start)
+            .filter(value => value.split(",")(7).toDouble < x_end)
+            .filter(value => value.split(",")(8).toDouble < y_start)
+            .filter(value => value.split(",")(8).toDouble >= y_end)
+          )
+        return query
+      }
+    }
+    else{
+      if(y_end >= y_start){
+        val query = df2
+          .flatMap(lines => lines.split("\n")
+            .filter(value => value.split(",")(7).toDouble >= x_end)
+            .filter(value => value.split(",")(7).toDouble < x_start)
+            .filter(value => value.split(",")(8).toDouble >= y_start)
+            .filter(value => value.split(",")(8).toDouble < y_end)
+          )
+        return query
+      }
+      else{
+        val query = df2
+          .flatMap(lines => lines.split("\n")
+            .filter(value => value.split(",")(7).toDouble >= x_end)
+            .filter(value => value.split(",")(7).toDouble < x_start)
+            .filter(value => value.split(",")(8).toDouble <= y_start)
+            .filter(value => value.split(",")(8).toDouble > y_end)
+          )
+        return query
+      }
+    }
+  }
+
+  def radiusQuery(df2 : RDD[String], lat1 : Double, lon1 : Double, km : Double): RDD[String] ={
+    val query = df2
+      .flatMap(lines => lines.split("\n")
+        .filter(value => {
+//          calculateDistanceInKm(lat1,lon1,value.split(",")(7).toDouble, value.split(",")(8).toDouble) < km
+
+          val lat2 = value.split(",")(7).toDouble
+          val lon2 = value.split(",")(8).toDouble
+
+          val AVERAGE_RADIUS_OF_EARTH_KM = 6371
+
+          val latDistance = Math.toRadians(lat1 - lat2)
+          val lngDistance = Math.toRadians(lon1 - lon2)
+          val sinLat = Math.sin(latDistance / 2)
+          val sinLng = Math.sin(lngDistance / 2)
+          val a = sinLat * sinLat +
+            (Math.cos(Math.toRadians(lat1)) *
+              Math.cos(Math.toRadians(lat2)) *
+              sinLng * sinLng)
+          val c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+          (AVERAGE_RADIUS_OF_EARTH_KM * c).toInt
+
+        } <= km)
       )
     return query
   }
